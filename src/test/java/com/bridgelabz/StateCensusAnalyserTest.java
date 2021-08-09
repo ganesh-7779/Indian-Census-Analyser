5c512914749a2743a23deb6ab8d88c26234313e6
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-08-09 23:57:44 +0530

    Refactor : Added TC1.1 WhenIncurrect_ThrownCustomException

--- src/test/java/com/bridgelabz/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/StateCensusAnalyserTest.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class StateCensusAnalyserTest {
+    public static final String STATECODES_CSVFILE = "D:\\fellowshipBreid\\indianCensusCsv\\src\\main\\resources\\IndiaStateCode.csv";
     public static final String STATECENSUS_CSVFILE = "D:\\fellowshipBreid\\indianCensusCsv\\src\\main\\resources\\IndiaStateCensusData.csv";
+    public static final String WRONG_FILE = "useless.txt";
+
     @Test
     public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
         try {
@@ -17,4 +20,14 @@ public class StateCensusAnalyserTest {
             e.printStackTrace();
         }
     }
+    @Test
+    public void GivenTheStateCensusCsvFile_IfDoesntExist_ShouldThrowCensusAnalyserException() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(WRONG_FILE, StateCensus.class);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE, e.type);
+        }
+    }
 }
+
